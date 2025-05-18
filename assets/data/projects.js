e402526cb48b9a909c28671e8f04fa5793d2e718
gautamkaran <karankumargautam.dev@gmail.com>
2025-05-18 12:21:28 +0530

    add stopwatch in peoject data array

--- assets/data/projects.js
+++ assets/data/projects.js
@@ -4,7 +4,15 @@ const projectsData = [
     img: "./assets/img/DigitalClock.png",
     description:
       "A simple digital clock displaying real-time hours, minutes, and seconds.",
-    liveLink: "./Digital Clock/index.html",
+    liveLink: "./Digital Clock/",
     githubLink: "https://github.com/gautamkaran/30Projects30days/tree/main/Digital%20Clock",
   },
+  {
+    title: "Stopwatch",
+    img: "./assets/img/stopwatch.png",
+    description: "A simple digital stopwatch with start, stop, and reset functionality.",
+    liveLink: "./Stopwatch",
+    githubLink: "https://github.com/gautamkaran/30Projects30days/tree/main/Stopwatch"
+  }
+
 ];
