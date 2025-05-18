cfe7c9ef98cf3b74a2ebc8eacbf66736c9da751d
gautamkaran <karankumargautam.dev@gmail.com>
2025-05-18 12:49:48 +0530

    convert project img into webp

--- assets/data/projects.js
+++ assets/data/projects.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: "Digital Clock",
-    img: "./assets/img/DigitalClock.png",
+    img: "./assets/img/DigitalClock.webp",
     description:
       "A simple digital clock displaying real-time hours, minutes, and seconds.",
     liveLink: "./Digital Clock/",
@@ -9,7 +9,7 @@ const projectsData = [
   },
   {
     title: "Stopwatch",
-    img: "./assets/img/stopwatch.png",
+    img: "./assets/img/stopwatch.webp",
     description: "A simple digital stopwatch with start, stop, and reset functionality.",
     liveLink: "./Stopwatch",
     githubLink: "https://github.com/gautamkaran/30Projects30days/tree/main/Stopwatch"
